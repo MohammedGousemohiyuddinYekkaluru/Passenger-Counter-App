635d28def637a55f413b95fcd6eec2c1d687db57
mohammed gouse <gousemohammed163@gmail.com>
2025-11-18 12:21:58 +0530

    Add functionality of save button and etc.,

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const countElement = document.querySelector('#count-el');
 const incrementBtn = document.querySelector('#increment-btn');
 const saveBtn = document.querySelector('#save-btn');
 const resetBtn = document.getElementById('reset-btn');
-const decrementBtn = document.getElementById('decrement-btn')
+const decrementBtn = document.getElementById('decrement-btn');
+const prevEntries = document.querySelector('.prev-entries');
+const resetEntryBtn = document.getElementById('reset-entries');
 let count = 0;
 
 incrementBtn.addEventListener('click', () => {
@@ -21,5 +23,11 @@ resetBtn.addEventListener('click', () => {
 })
 
 saveBtn.addEventListener('click', () => {
-    console.log(count);
+    prevEntries.textContent += ` ${count} - `;
+    count = 0
+    countElement.textContent = count;
+})
+
+resetEntryBtn.addEventListener('click', () => {
+    prevEntries.textContent = ""
 })
